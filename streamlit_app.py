de62d0145bb7757a824946858f2ffecb015152f6
snowjaeu <141299999+snowjaeu@users.noreply.github.com>
2023-08-03 14:23:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # 포함할 과일을 고를 수 있도록 선택 목록을 여기에 넣자 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) 
+
+streamlit.dataframe(my_fruit_list)
+
+
